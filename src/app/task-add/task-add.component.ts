8967213ff2d01508c5b3bbd98b15bc57623f7920
Uyanhewajanadhi <uyanhewajandhi@gmail.com>
2022-01-13 13:57:20 +0530

    input_validation

--- src/app/task-add/task-add.component.ts
+++ src/app/task-add/task-add.component.ts
@@ -1,15 +1,18 @@
-import {Component} from '@angular/core';
-import {FormControl} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {TaskStorageService} from "../task-storage.service";
 import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-task-add',
   templateUrl: './task-add.component.html',
   styleUrls: ['./task-add.component.css']
 })
-export class TaskAddComponent {
+export class TaskAddComponent implements OnInit {
 
+  form: FormGroup;
+  submitted = false;
   /**
    * Task title form field
    */
@@ -19,15 +22,30 @@ export class TaskAddComponent {
    *  Task note form field
    */
   note = new FormControl('');
+  d = "hi"
 
-  constructor(private storage: TaskStorageService, private router: Router) {
+  constructor(private storage: TaskStorageService, private router: Router, private formBuilder: FormBuilder) {
   }
 
+  ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        title: ['', Validators.required],
+      });
+  }
   /**
    * Create a task a redirect to the todo list
    */
   createTask() {
     this.storage.add(this.title.value, this.note.value);
     this.router.navigate(['/tasks'])
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+  
+  t() {
+    alert("hello")
   }
 }
--- src/app/task-storage.service.spec.ts
+++ src/app/task-storage.service.spec.ts
@@ -5,43 +5,36 @@ import { TaskStorageService } from './task-storage.service';
 describe('TaskStorageService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: TaskStorageService = TestBed.get(TaskStorageService);
+  const service: TaskStorageService = TestBed.get(TaskStorageService);
+
+  it('service should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should load tasks from json file', () => {
-    const service: TaskStorageService = TestBed.get(TaskStorageService);
+  it('should load tasks from file', () => {
     expect(service.getTasks()).toBeTruthy();
   });
 
-  it('should count the number of tasks, after adding and removing', () => {
-      const service: TaskStorageService = TestBed.get(TaskStorageService); 
-      expect(service.getTasks().length).toBe(5);
-    });
-
   it('should get the task by id', () => {
-    const service: TaskStorageService = TestBed.get(TaskStorageService);
     //expect(service.getTaskById(1)).toBeTruthy();
     expect(service.getTaskById(3).title).toBe("Buy groceries");
   });
-
+  
   it('should update a tasks', () => {
-    const service: TaskStorageService = TestBed.get(TaskStorageService);
     expect(service.updateTaskById(2,"Buy magazine","Magazine name is BitHit!")).toBeTruthy();
     expect(service.getTaskById(2).title).toBe("Buy magazine");
   });
 
   it('should returns highest task id from the list', () => {
-    const service: TaskStorageService = TestBed.get(TaskStorageService);
     expect(service.getTaskBWithHighestId()).toBe(5);
   });
 
   it('should delete a tasks by Id', () => {
-    const service: TaskStorageService = TestBed.get(TaskStorageService);
     expect(service.deleteTaskById(2)).toBeTruthy();
     //expect(service.getTasks.length).toBe(4);
   });
-  
 
+  it('should count the number of tasks, after adding and removing', () => {
+    expect(service.getTasks().length).toBe(5);
+  });
 });
