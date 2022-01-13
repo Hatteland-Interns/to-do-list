48a045dd24932cbd0b85dbb50fc52483d7441e34
Dharana Rodrigo <dharanar@99x.io>
2022-01-13 13:56:19 +0530

    communication between API and frontend is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { TaskEditComponent } from './task-edit/task-edit.component';
 import { TaskViewComponent } from './task-view/task-view.component';
 import { TaskAddComponent } from './task-add/task-add.component';
+import { HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { TaskAddComponent } from './task-add/task-add.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/task-storage.service.ts
+++ src/app/task-storage.service.ts
@@ -1,22 +1,22 @@
-import {Injectable} from '@angular/core';
+import { Injectable, OnDestroy } from "@angular/core";
 
-import {init_tasks} from '../assets/todo-list.json';
-import {Task} from "../app/shared/models/task.model";
+import { Task } from "../app/shared/models/task.model";
+import { HttpClient } from "@angular/common/http";
+import { Subscription } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-export class TaskStorageService {
-
+export class TaskStorageService implements OnDestroy {
   tasks: Task[] = [];
+  private subscription: Subscription;
 
   /**
    * Whether data have already been loaded from storage
    */
   initialized: boolean = false;
 
-  constructor() {
-  }
+  constructor(private http: HttpClient) {}
 
   /* Returns all tasks */
   getTasks(): Task[] {
@@ -45,32 +45,31 @@ export class TaskStorageService {
 
     return task;
   }
-  
-   /* Get the task with Highest Id */
-   getTaskBWithHighestId(): number {
-      let highest: number = 0;
-      this.init();
-      this.tasks.forEach(function (current_task: Task) {
 
-        if (current_task.id < highest) {
-          return;
-        }
+  /* Get the task with Highest Id */
+  getTaskBWithHighestId(): number {
+    let highest: number = 0;
+    this.init();
+    this.tasks.forEach(function (current_task: Task) {
+      if (current_task.id < highest) {
+        return;
+      }
 
-        highest = current_task.id;
-      });
+      highest = current_task.id;
+    });
 
-      return highest;
+    return highest;
   }
 
   /* Delete task by Id */
-  deleteTaskById(id){
+  deleteTaskById(id) {
     let remaining_tasks: Task[] = [];
     for (let i = 0; i < this.tasks.length; i++) {
       var current_task = this.tasks[i];
 
       // we found the task to remove, we do not include it in our new array
       if (id == current_task.id) {
-        console.log('Skipping tash[' + current_task.title + ']');
+        console.log("Skipping tash[" + current_task.title + "]");
         continue;
       }
 
@@ -78,27 +77,28 @@ export class TaskStorageService {
     }
     this.tasks = remaining_tasks;
     return true;
-}
+  }
 
-  
   /**
-   * Load tasks from json file
+   * Load tasks from the API
    */
   init() {
     if (this.initialized) {
-      console.log('Already initialized');
+      console.log("Already initialized");
       return;
     }
-    console.log('Loading data from json file');
+    console.log("Loading data from json file");
 
-    for (let i = 0; i < init_tasks.length; i++) {
-      this.tasks.push(
-        new Task(
-          init_tasks[i]['title'],
-          init_tasks[i]['note'],
-          init_tasks[i]['id'])
-      );
-    }
+    this.subscription = this.http
+      .get("https://localhost:44378/api/TodoItems")
+      .subscribe((tasks: any[]) => {
+        console.log(tasks);
+        for (let i = 0; i < tasks.length; i++) {
+          this.tasks.push(
+            new Task(tasks[i]["title"], tasks[i]["note"], tasks[i]["id"])
+          );
+        }
+      });
 
     this.initialized = true;
   }
@@ -107,9 +107,12 @@ export class TaskStorageService {
    * @param title
    * @param note
    */
-   add(title, note) {
+  add(title, note) {
     let task = new Task(title, note, this.getTaskBWithHighestId() + 1);
     this.tasks.push(task);
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
