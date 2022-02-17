f41ad5decabe4c17b2be77344dba07f8dd929c70
Uyanhewajanadhi <uyanhewajanadhi@gmail.com>
2022-02-17 09:15:02 +0530

    Uncommented failing tests

--- src/app/task-storage.service.spec.ts
+++ src/app/task-storage.service.spec.ts
@@ -22,37 +22,37 @@ describe('TaskStorageService', () => {
       });
 
 
-        // it('getTasks() should call http Get method for the given route', () => {     
-        //   task = {id : 1, title: "Buy the magazine", note: "Bit Hit!"};
-        //   service.getTasks().subscribe((t)=>{
-        //     expect(t).toEqual(tasks);
-        //   });
-        // });
+        it('getTasks() should call http Get method for the given route', () => {     
+          task = {id : 1, title: "Buy the magazine", note: "Bit Hit!"};
+          service.getTasks().subscribe((t)=>{
+            expect(t).toEqual(tasks);
+          });
+        });
 
-        // it('getTaskById() should get the task by id', () => {     
-        //   task = {id : 1, title: "Buy the magazine", note: "Bit Hit!"};
-        //   service.getTaskById(1).subscribe((t)=>{
-        //     expect(t).toEqual(task);
-        //   });
-        // });
+        it('getTaskById() should get the task by id', () => {     
+          task = {id : 1, title: "Buy the magazine", note: "Bit Hit!"};
+          service.getTaskById(1).subscribe((t)=>{
+            expect(t).toEqual(task);
+          });
+        });
 
-        // it('updateTaskById() should update a tasks', () => {     
-        //   task = {id : 1, title: "Buy the magazine", note: "Bit Hit!"};
-        //   let temp :Task;
-        //   temp.id=1;
-        //   temp.title="Todo Tasks";
-        //   temp.note="Complete angular app";
-        //   service.updateTaskById(temp).subscribe((t)=>{
-        //     expect(t).toEqual(task);
-        //   });
-        // });
+        it('updateTaskById() should update a tasks', () => {     
+          task = {id : 1, title: "Buy the magazine", note: "Bit Hit!"};
+          let temp :Task;
+          temp.id=1;
+          temp.title="Todo Tasks";
+          temp.note="Complete angular app";
+          service.updateTaskById(temp).subscribe((t)=>{
+            expect(t).toEqual(task);
+          });
+        });
 
-        // it('deleteTaskById() should delete a tasks by Id', () => {     
-        //   task = {id : 1, title: "Buy the magazine", note: "Bit Hit!"};
-        //   service.deleteTaskById(1).subscribe((t)=>{
-        //     expect(t).toEqual(task);
-        //     expect(service.getTasks.length).toBe(0);
-        //   });
-        // });
+        it('deleteTaskById() should delete a tasks by Id', () => {     
+          task = {id : 1, title: "Buy the magazine", note: "Bit Hit!"};
+          service.deleteTaskById(1).subscribe((t)=>{
+            expect(t).toEqual(task);
+            expect(service.getTasks.length).toBe(0);
+          });
+        });
 
 });
